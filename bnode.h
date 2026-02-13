36240b1050a9b00a7bc4b347a420aa3410302995
Michael0131 <michaelj0131@byui.edu>
2026-02-12 18:47:35 -0700

    Set up done, Assignments are in comments, Michael did all set up

--- bnode.h
+++ bnode.h
@@ -15,151 +15,176 @@
  *        BNode         : A class representing a BNode
  *    Additionally, it will contain a few functions working on Node
  * Author
- *    <your names here>
+ *    Michael, James, Brayden
  ************************************************************************/
+
+
 
 #pragma once
 
-#include <iostream>  // for OFSTREAM
 #include <cassert>
+#include <cstddef>  // size_t
+#include <utility>  // std::move, std::swap
 
 /*****************************************************************
  * BNODE
- * A single node in a binary tree.  Note that the node does not know
- * anything about the properties of the tree so no validation can be done.
  *****************************************************************/
 template <class T>
 class BNode
 {
 public:
-   // 
-   // Construct
-   //
-   BNode()
-   {
-      pLeft = pRight = this;
-   }
-   BNode(const T& t)
-   {
-      pLeft = pRight = this;
-   }
-   BNode(T&& t)
-   {
-      pLeft = pRight = this;
-   }
+    // ---------- Michael Code To Complete ----------
+    // Construct
+    BNode() : pLeft(nullptr), pRight(nullptr), pParent(nullptr), data() {}
+    BNode(const T& t) : pLeft(nullptr), pRight(nullptr), pParent(nullptr), data(t) {}
+    BNode(T&& t) : pLeft(nullptr), pRight(nullptr), pParent(nullptr), data(std::move(t)) {}
 
-   //
-   // Data
-   //
-   BNode <T>* pLeft;
-   BNode <T>* pRight;
-   BNode <T>* pParent;
-   T data;
+    // Data
+    BNode<T>* pLeft;
+    BNode<T>* pRight;
+    BNode<T>* pParent;
+    T data;
 };
 
 /*******************************************************************
  * SIZE BTREE
- * Return the size of a b-tree under the current node
  *******************************************************************/
 template <class T>
-inline size_t size(const BNode <T> * p)
+inline size_t size(const BNode<T>* p)
 {
-   return 99;
-}
-
-
-/******************************************************
- * ADD LEFT
- * Add a node to the left of the current node
- ******************************************************/
-template <class T>
-inline void addLeft(BNode <T> * pNode, BNode <T> * pAdd)
-{
-
+    // ---------- James Code To Complete ----------
+    return 99;
 }
 
 /******************************************************
- * ADD RIGHT
- * Add a node to the right of the current node
+ * ADD LEFT (tree)
  ******************************************************/
 template <class T>
-inline void addRight (BNode <T> * pNode, BNode <T> * pAdd)
+inline void addLeft(BNode<T>* pNode, BNode<T>* pAdd)
 {
-
+    // ---------- Brayden Code To Complete ----------
+    
 }
 
 /******************************************************
- * ADD LEFT
- * Add a node to the left of the current node
+ * ADD RIGHT (tree)
  ******************************************************/
 template <class T>
-inline void addLeft (BNode <T> * pNode, const T & t)
+inline void addRight(BNode<T>* pNode, BNode<T>* pAdd)
 {
-
-}
-
-template <class T>
-inline void addLeft(BNode <T>* pNode, T && t)
-{
-
+    // ---------- Brayden Code To Complete ----------
+    
 }
 
 /******************************************************
- * ADD RIGHT
- * Add a node to the right of the current node
+ * ADD LEFT (value)
  ******************************************************/
 template <class T>
-void addRight (BNode <T> * pNode, const T & t)
+inline void addLeft(BNode<T>* pNode, const T& t)
 {
-
+    // ---------- Brayden Code To Complete ----------
+    
 }
 
 template <class T>
-void addRight(BNode <T>* pNode, T && t)
+inline void addLeft(BNode<T>* pNode, T&& t)
 {
+    // ---------- Brayden Code To Complete ----------
+    
+}
 
+/******************************************************
+ * ADD RIGHT (value)
+ ******************************************************/
+template <class T>
+void addRight(BNode<T>* pNode, const T& t)
+{
+    // ---------- James Code To Complete ----------
+    
+    
+}
+
+template <class T>
+void addRight(BNode<T>* pNode, T&& t)
+{
+    // ---------- James Code To Complete ----------
+    
+    
 }
 
 /*****************************************************
- * DELETE BINARY TREE
- * Delete all the nodes below pThis including pThis
- * using postfix traverse: LRV
+ * CLEAR (post-order LRV)
  ****************************************************/
 template <class T>
-void clear(BNode <T> * & pThis)
+void clear(BNode<T>*& pThis)
 {
+    // ---------- Michael Code To Complete ----------
+    if (pThis == nullptr)
+        return;
 
+    clear(pThis->pLeft);
+    clear(pThis->pRight);
+
+    delete pThis;
+    pThis = nullptr;
+    
 }
 
 /***********************************************
  * SWAP
- * Swap the list from LHS to RHS
- *   COST   : O(1)
  **********************************************/
 template <class T>
-inline void swap(BNode <T>*& pLHS, BNode <T>*& pRHS)
+inline void swap(BNode<T>*& pLHS, BNode<T>*& pRHS)
 {
-
+    // ---------- Michael Code To Complete ----------
+    std::swap(pLHS, pRHS);
+    
 }
 
 /**********************************************
- * COPY BINARY TREE
- * Copy pSrc->pRight to pDest->pRight and
- * pSrc->pLeft onto pDest->pLeft
+ * COPY (deep copy, fix parent pointers)
  *********************************************/
 template <class T>
-BNode <T> * copy(const BNode <T> * pSrc)
+BNode<T>* copy(const BNode<T>* pSrc)
 {
-   return new BNode<T>;
+    // ---------- Michael Code To Complete ----------
+    if (pSrc == nullptr)
+        return nullptr;
+
+    BNode<T>* pNew = new BNode<T>(pSrc->data);
+
+    pNew->pLeft = copy(pSrc->pLeft);
+    if (pNew->pLeft)
+        pNew->pLeft->pParent = pNew;
+
+    pNew->pRight = copy(pSrc->pRight);
+    if (pNew->pRight)
+        pNew->pRight->pParent = pNew;
+
+    // root's parent is nullptr by constructor
+    return pNew;
 }
 
-/**********************************************
- * assign
- * copy the values from pSrc onto pDest preserving
- * as many of the nodes as possible.
- *********************************************/
 template <class T>
-void assign(BNode <T> * & pDest, const BNode <T>* pSrc)
+void assign(BNode<T>*& pDest, const BNode<T>* pSrc, BNode<T>* pParent = nullptr)
 {
+    // ---------- Michael Code To Complete ----------
+    if (pSrc == nullptr)
+    {
+        clear(pDest);
+        return;
+    }
 
+    // create or update this node
+    if (pDest == nullptr)
+        pDest = new BNode<T>(pSrc->data);
+    else
+        pDest->data = pSrc->data;
+
+    pDest->pParent = pParent;
+
+    // recurse into children
+    assign(pDest->pLeft, pSrc->pLeft, pDest);
+    assign(pDest->pRight, pSrc->pRight, pDest);
 }
+
